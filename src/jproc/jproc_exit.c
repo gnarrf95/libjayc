f36d8374dd15a86607fa2bd9a024ae20715d4538
Manuel Nadji <manuel.nadji@gmail.com>
2020-10-01 12:49:29 +0200

    Added jproc component.

---
+++ src/jproc/jproc_exit.c
@@ -0,0 +1,44 @@
+/**
+ * @file jproc_exit.c
+ * @author Manuel Nadji (https://github.com/gnarrf95)
+ * 
+ * @brief Implements exit functionality for jproc.
+ * 
+ * @date 2020-10-01
+ * @copyright Copyright (c) 2020 by Manuel Nadji
+ * 
+ */
+
+#include <jayc/jproc.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct __jproc_exit_function_pair
+{
+  jproc_exit_handler_t handler;
+  void *ctx;
+} jproc_exit_function_t;
+
+static jproc_exit_function_t exit_function = { NULL, NULL };
+
+//------------------------------------------------------------------------------
+//
+void jproc_exit(int exit_value)
+{
+  if(exit_function.handler)
+  {
+    exit_function.handler(exit_value, exit_function.ctx);
+  }
+
+  exit(exit_value);
+}
+
+//------------------------------------------------------------------------------
+//
+int jproc_exit_setHandler(jproc_exit_handler_t handler, void *ctx)
+{
+  exit_function.handler = handler;
+  exit_function.ctx = ctx;
+
+  return true;
+}

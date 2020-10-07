3c7c3f0f90d91fb97d0c50e914ac2c237d13e969
Manuel Nadji <manuel.nadji@gmail.com>
2020-10-07 20:12:13 +0200

    Added functioning jutil_cli module.

---
+++ inc/jayc/jutil_cli.h
@@ -0,0 +1,65 @@
+/**
+ * @file jutil_cli.h
+ * @author Manuel Nadji (https://github.com/gnarrf95)
+ * 
+ * @brief Interface for managing CLI input.
+ * 
+ * @date 2020-10-07
+ * @copyright Copyright (c) 2020 by Manuel Nadji
+ * 
+ */
+
+#ifndef INCLUDE_JUTIL_CLI_H
+#define INCLUDE_JUTIL_CLI_H
+
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief Handler that gets called when CLI input arrives.
+ * 
+ * @param args      Array of arguments.
+ * @param arg_size  Size of array.
+ * 
+ * @return          NOT USED YET ...
+ */
+typedef int(*jutil_cli_cmdHandler_t)(const char **args, size_t arg_size, void *ctx);
+
+typedef struct __jutil_cli_session jutil_cli_t;
+
+/**
+ * @brief Initializes session.
+ * 
+ * @param handler Handler function to be called, when input is recieved.
+ * @param ctx     Session context pointer.
+ * 
+ * @return        New session object.
+ * @return        @c NULL , if error occured.
+ */
+jutil_cli_t *jutil_cli_init(jutil_cli_cmdHandler_t handler, void *ctx);
+
+/**
+ * @brief Frees memory of session.
+ * 
+ * @param session Session object to free.
+ */
+void jutil_cli_free(jutil_cli_t *session);
+
+/**
+ * @brief Read input and parse to handler.
+ * 
+ * @param session Session object.
+ * 
+ * @return        @c true , if successful.
+ * @return        @c false , if error occured.
+ */
+int jutil_cli_run(jutil_cli_t *session);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INCLUDE_JUTIL_CLI_H */
